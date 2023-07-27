b1206dc66ca406cce4b9e1ea8cecc53ea7110df6
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-07-27 19:03:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function decimalToBinary(num) {
-  //Write you code here
-return num.toString(2);
+unction decimalToBinary(num) {
+  // Write your code here
+	let binStr = "";
+	while(num !== 0){
+		let rem = num%2;
+		binStr+=rem;
+		num = Math.floor(num/2);
+	}
+	return binStr.split("").reverse().join("");
 }
- 
-console.log(decimalToBinary(num));
+
+window.decimalToBinary = decimalToBinary;
